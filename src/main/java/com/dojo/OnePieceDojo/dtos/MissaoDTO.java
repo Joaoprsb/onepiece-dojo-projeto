409a01a8dfea20d72763b6ef2591b608e6957034
Joao_barboza <jprsbarboza@gmail.com>
2026-06-01 14:02:20 -0300

    feat: adiciona validações obrigatórias em MissaoDTO

--- src/main/java/com/dojo/OnePieceDojo/dtos/MissaoDTO.java
+++ src/main/java/com/dojo/OnePieceDojo/dtos/MissaoDTO.java
@@ -5,6 +5,7 @@ import com.dojo.OnePieceDojo.entities.Missao;
 import com.dojo.OnePieceDojo.enums.ClassificacaoMissao;
 import com.dojo.OnePieceDojo.enums.StatusMissao;
 import com.dojo.OnePieceDojo.enums.TipoMissao;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +18,16 @@ import lombok.Setter;
 public class MissaoDTO {
 
     private Long id;
+
+    @NotNull(message = "A classificação da missão é obrigatória.")
     private ClassificacaoMissao classificacaoMissao;
+
+    @NotNull(message = "O tipo da missão é obrigatório.")
     private TipoMissao tipoMissao;
+
+    @NotNull(message = "O status da missão é obrigatório.")
     private StatusMissao statusMissao;
+
     private Long pirataId;
 
     public MissaoDTO(Missao missao) {
